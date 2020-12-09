779496287ea7561273239ab5c81e73a7377a2bde
Bruno Gois <brunovgois@gmail.com>
2020-12-09 17:40:21 -0300

    criar header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import "./index.css"
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <nav>
+        <ul className="header-list">
+          <li>
+            <Link to="/" className="header-links">Home</Link>
+          </li>
+          <li>
+            <Link to="/alunos" className="header-links">Alunos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
