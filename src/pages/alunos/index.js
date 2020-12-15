c9806e42e2962612818829d033ea8f224b2d0ee0
Bruno Gois <brunovgois@gmail.com>
2020-12-15 20:54:19 -0300

    deletar e coluna id para alunos

--- src/pages/alunos/index.js
+++ src/pages/alunos/index.js
@@ -12,20 +12,36 @@ export default function Alunos() {
     const fetchData = async () => {
       const result = await axios.get('/aluno');
       setAlunos(result.data)
-      console.log(result)
     };
-
     fetchData();
-  }, []) //2º argumento define as variaveis que o hooke depende. [] = fetchData so vai ser chamado quando o componente é montado
+  }, [])
 
   const columns = [
+    { title: 'ID', field: 'id' },
     { title: 'Nome', field: 'name' },
     { title: 'Classe', field: 'classe' }
   ]
 
+  const handleDelete = (alunoId) => {
+    axios.delete(`aluno/${alunoId}`).then(() => {
+      const updatedAlunosList = alunos.content.filter(aluno => aluno.id !== alunoId)
+      console.log(updatedAlunosList)
+      
+      setAlunos(updatedAlunosList)
+    })
+  }
+
   return (
     <div className="App">
-      <Table title="Alunos" columns={columns} data={alunos.content}/>
+      <Table
+        title="Alunos"
+        columns={columns}
+        data={alunos.content}
+        deleteTooltip={"aluno"}
+        handleDelete={handleDelete}
+      />
     </div>
   )
+
 }
+
