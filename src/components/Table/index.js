9b8ab7130383ee20d97fab36f71d73ed4337526f
Bruno Gois <brunovgois@gmail.com>
2020-12-15 23:22:43 -0300

    criar aluno

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,4 +1,4 @@
-import { React, forwardRef } from 'react'
+import { React, forwardRef, useState } from 'react'
 import MaterialTable from 'material-table'
 
 import ChevronLeft from '@material-ui/icons/ChevronLeft';
@@ -9,38 +9,46 @@ import FirstPage from '@material-ui/icons/FirstPage';
 import LastPage from '@material-ui/icons/LastPage';
 import Search from '@material-ui/icons/Search';
 import DeleteOutline from '@material-ui/icons/DeleteOutline'
+import AddIcon from '@material-ui/icons/Add';
 
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
 
 export default function Table(props) {
 
-    const tableIcons = {
-        Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-        Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-        FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-        LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-        NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-        PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-        Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-        ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    
-      };
+  const tableIcons = {
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  };
 
   return (
-      <MaterialTable
-        title={props.title}
-        icons={tableIcons}
-        columns={props.columns}
-        data={props.data}
-        actions={[
-          {
-            icon: DeleteOutline,
-            tooltip: `Deletar ${props.deleteTooltip}`,
+    <MaterialTable
+      title={props.title}
+      icons={tableIcons}
+      columns={props.columns}
+      data={props.data}
+      actions={[
+        {
+          icon: DeleteOutline,
+          tooltip: `Deletar ${props.tooltip}`,
           onClick: (event, rowData) => props.handleDelete(rowData.id)
-          }
-        ]}
-        options={{
-          actionsColumnIndex: -1
-        }}
-      />
-    )
+        },
+        {
+          icon: AddIcon,
+          tooltip: `Adicionar ${props.tooltip}`,
+          isFreeAction: true,
+          onClick: (event) => props.handleAdd()
+        }
+      ]}
+      options={{
+        actionsColumnIndex: -1
+      }}
+    />
+  )
 }
--- src/pages/alunos/index.js
+++ src/pages/alunos/index.js
@@ -2,11 +2,23 @@ import { React, useEffect, useState } from 'react'
 import axios from '../../services/httpService'
 import Table from '../../components/Table'
 
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+
 import "./index.css"
 
 export default function Alunos() {
 
   const [alunos, setAlunos] = useState({})
+
+  const [open, setOpen] = useState(false)
+
+  const [studentName, setStudentName] = useState("")
+  const [studentClass, setStudentClass] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,20 +37,78 @@ export default function Alunos() {
   const handleDelete = (alunoId) => {
     axios.delete(`aluno/${alunoId}`).then(() => {
       const updatedAlunosList = alunos.content.filter(aluno => aluno.id !== alunoId)
-      console.log(updatedAlunosList)
-      
+
       setAlunos(updatedAlunosList)
     })
   }
+  const handleCreate = () => {
+    axios.post('aluno', {
+      name: studentName,
+      classe: studentClass
+    }).then((response) => {
+      setAlunos(...alunos.content, response.data)
+      setOpen(false);
+    })
+  }
+
+  const handleNameChange = (e) => {
+    setStudentName(e.target.value)
+  }
+
+  const handleClassChange = (e) => {
+    setStudentClass(e.target.value)
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setStudentName("")
+    setStudentClass("")
+
+    setOpen(false);
+  };
+
+
 
   return (
     <div className="App">
+
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Adicionar Aluno</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            label="Nome"
+            fullWidth
+            onChange={handleNameChange}
+          />
+          <TextField
+            margin="dense"
+            label="Classe"
+            fullWidth
+            onChange={handleClassChange}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={handleCreate} color="primary">
+            Adicionar
+          </Button>
+        </DialogActions>
+      </Dialog>
+
       <Table
         title="Alunos"
         columns={columns}
         data={alunos.content}
-        deleteTooltip={"aluno"}
+        tooltip={"aluno"}
         handleDelete={handleDelete}
+        handleAdd={handleClickOpen}
       />
     </div>
   )
